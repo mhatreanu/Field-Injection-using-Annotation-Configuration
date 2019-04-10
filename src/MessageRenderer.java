02f7fef19e1d7bc024b9e1d5ce3c2563be9ca99c
Mhatre <anushree-shirish.mhatre@capgemini.com>
2019-04-10 18:30:21 +0530

    field

---
+++ src/MessageRenderer.java
@@ -0,0 +1,18 @@
+package com.capgemini.renderer;
+import com.capgemini.provider.MessageProvider;
+import com.capgemini.provider.implementer.HelloMessageProvider;
+import com.capgemini.provider.GmMessageProvider;
+import org.springframework.stereotype.*;
+import org.springframework.beans.factory.annotation.*;
+
+@Component
+public class MessageRenderer{
+		
+		 @Autowired
+		private MessageProvider provider;
+		
+		public void render(){
+		
+			System.out.println(provider.printMessage());
+		}
+}
